6e199a2211cd554c28d945dcb29b4f8315b9f96b
Shinaz <shinazazeez@gmail.com>
2022-08-19 18:21:21 +0530

    fix

--- backend/app.js
+++ backend/app.js
@@ -104,7 +104,9 @@ var permitCrossDomainRequests = function (req, res, next) {
 var routesApi = require('./routes/index');
 
 var app = express();
-app.use(cors());
+app.use(cors({
+  origin: '*'
+}));
 app.use(permitCrossDomainRequests);
 app.use(bodyParser.json({ limit: '50mb' }));
 app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
